a7dc82b5f047fad2d5ed1cc552e270fad0158d80
Saniya Sungenova <sungenovy@gmail.com>
2020-10-25 22:38:39 +0600

    Script

---
+++ DBScript.sql
@@ -0,0 +1,11 @@
+﻿create database UserManagementDB
+go
+use UserManagementDB
+go
+create table Users
+(   
+   IIN varchar(50) primary key,
+   FirstName nvarchar(50) not null,
+   LastName nvarchar(50) not null,
+   BirthDate date not null
+)
